02ce24fc13f3e4b23b08071df954c0da286ae9ff
Thom�s Antonielli  Veloso <52741832803@>
2025-07-30 14:16:17 -0300

    SUBINDO AS ATIVIDADES DA AULA DE PWBE DO DIA 30/07

--- AULA DIA 30_07/at7.py
+++ AULA DIA 30_07/at7.py
@@ -1,6 +1,6 @@
 import time
 
 while True:
-    hora_atual = time.strftime("%H:%M:%S", time.localtime())
+    hora_atual = time.strftime("%H:%M")
     print(f"Hora atual: {hora_atual}")
     time.sleep(1)
